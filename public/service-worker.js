0fba28620d1f05468e9920def903e0897b23058b
Stephen Price <stephen_lprice@live.com>
2021-03-06 03:40:30 -0600

    added service worker activation

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 const FILES_TO_CACHE =  [
   "/",
   "/index.html",
-  "/manifest.webmanifest", // decide if I will create one later
+  // "/manifest.webmanifest", // decide if I will create one later
   "/index.js",
   "/styles.css",
   "/icons/icon-192x192.png",
@@ -9,7 +9,7 @@ const FILES_TO_CACHE =  [
 ];
 
 const STATIC_CACHE = 'static-cache-v1';
-const DATA_CACHE_ = 'data-cache-v1';
+const DATA_CACHE = 'data-cache-v1';
 
 // installing service workers
 self.addEventListener("install", (event) => {
@@ -29,3 +29,28 @@ self.addEventListener("install", (event) => {
       .then(() => self.skipWaiting())
   );
 });
+
+// removes old caches not listed on this file
+self.addEventListener("activate", event => {
+  const CACHES = [STATIC_CACHE, DATA_CACHE];
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames => {
+        // array of old cache names not included in CACHES
+        return cacheNames.filter(
+          cacheName => !CACHES.includes(cacheName)
+        );
+      })
+      .then(cachesToDelete => {
+        return Promise.all(
+          // remove each cache name included in previous array
+          cachesToDelete.map(cacheToDelete => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
