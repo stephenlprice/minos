ff77926be1f495027d42e54f0aef85fc7a02bd43
Stephen Price <stephen_lprice@live.com>
2021-03-06 03:24:40 -0600

    refactored index.html and installed service workers for data and static caches

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,13 +8,24 @@ const FILES_TO_CACHE =  [
   "/icons/icon-512x512.png"
 ];
 
-const CACHE_NAME = 'static-cache-v1';
-const DATA_CACHE_NAME = 'data-cache-v1';
+const STATIC_CACHE = 'static-cache-v1';
+const DATA_CACHE_ = 'data-cache-v1';
 
-// install the service worker
+// installing service workers
 self.addEventListener("install", (event) => {
-  // pre caching transaction data
+  // pre caching /api/transaction data
   event.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+    caches
+      .open(DATA_CACHE)
+      .then((cache) => cache.add("/api/transaction"))
+      .then(() => self.skipWaiting())
+  );
+
+  // pre caching all static assets
+  event.waitUntil(
+    caches
+      .open(STATIC_CACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
   );
 });
